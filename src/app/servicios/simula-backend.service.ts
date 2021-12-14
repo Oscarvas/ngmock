34b1380b70c28a5a00301764ce70f693956c20bd
Oscar Vasquez <oscardavid.vp@gmail.com>
2021-12-14 02:45:59 +0100

    llamada api cabecera y ayuda

--- src/app/servicios/simula-backend.service.ts
+++ src/app/servicios/simula-backend.service.ts
@@ -1,39 +1,29 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MAPA_PHNMSIPA } from '../material/pdas';
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry, tap } from 'rxjs/operators';
-
-const datos: MAPA_PHNMSIPA[] = [
-  {'SEL':'','ANO':'2021','ENTIDAD':'101','RECIBO':'2023025525','SIT':'I','MOD_PAGO':'2','ENT_PAGO':2095,'SUC_PAGO':3132,'FECHA_DESDE':20210301,'FECHA_HASTA':20210331,'TIP_PAGO':'NOMINA NORMAL'},
-  {'SEL':'','ANO':'2021','ENTIDAD':'101','RECIBO':'1022025499','SIT':'P','MOD_PAGO':'2','ENT_PAGO':2095,'SUC_PAGO':3132,'FECHA_DESDE':20210301,'FECHA_HASTA':20210331,'TIP_PAGO':'NOMINA NORMAL'},
-  {'SEL':'','ANO':'2021','ENTIDAD':'101','RECIBO':'6021025527','SIT':'P','MOD_PAGO':'2','ENT_PAGO':2095,'SUC_PAGO':3132,'FECHA_DESDE':20210301,'FECHA_HASTA':20210331,'TIP_PAGO':'NOMINA NORMAL'},
-  {'SEL':'','ANO':'2020','ENTIDAD':'101','RECIBO':'6032025562','SIT':'P','MOD_PAGO':'2','ENT_PAGO':2095,'SUC_PAGO':3132,'FECHA_DESDE':20210301,'FECHA_HASTA':20210331,'TIP_PAGO':'NOMINA NORMAL'},
-  {'SEL':'','ANO':'2020','ENTIDAD':'101','RECIBO':'8231025504','SIT':'P','MOD_PAGO':'2','ENT_PAGO':2095,'SUC_PAGO':3132,'FECHA_DESDE':20210301,'FECHA_HASTA':20210331,'TIP_PAGO':'PAGA EXTRA'},
-  {'SEL':'','ANO':'2020','ENTIDAD':'101','RECIBO':'2031025507','SIT':'P','MOD_PAGO':'2','ENT_PAGO':2095,'SUC_PAGO':3132,'FECHA_DESDE':20210301,'FECHA_HASTA':20210331,'TIP_PAGO':'NOMINA NORMAL'},
-  {'SEL':'','ANO':'2020','ENTIDAD':'101','RECIBO':'9030025455','SIT':'P','MOD_PAGO':'2','ENT_PAGO':2095,'SUC_PAGO':3132,'FECHA_DESDE':20210301,'FECHA_HASTA':20210331,'TIP_PAGO':'NOMINA NORMAL'},
-  {'SEL':'','ANO':'2020','ENTIDAD':'101','RECIBO':'5029025489','SIT':'P','MOD_PAGO':'2','ENT_PAGO':2095,'SUC_PAGO':3132,'FECHA_DESDE':20210301,'FECHA_HASTA':20210331,'TIP_PAGO':'NOMINA NORMAL'},
-  {'SEL':'','ANO':'2020','ENTIDAD':'101','RECIBO':'9028025508','SIT':'P','MOD_PAGO':'2','ENT_PAGO':2095,'SUC_PAGO':3132,'FECHA_DESDE':20210301,'FECHA_HASTA':20210331,'TIP_PAGO':'NOMINA NORMAL'},
-  {'SEL':'','ANO':'2020','ENTIDAD':'101','RECIBO':'7027025464','SIT':'P','MOD_PAGO':'2','ENT_PAGO':2095,'SUC_PAGO':3132,'FECHA_DESDE':20210301,'FECHA_HASTA':20210331,'TIP_PAGO':'NOMINA NORMAL'}
-];
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class SimulaBackendService {
-  private apiUrl = `http://localhost:5000/listado`;
+  private apiUrl = `http://localhost:5000/`;
   constructor(private http: HttpClient) { }
 
-  getListadoSIPA (): MAPA_PHNMSIPA[]{
-    return datos;
-  }
   // list(): Observable<any>{
   //   return this.http.get(this.apiUrl);
   // }
   list(): Observable<MAPA_PHNMSIPA[]>{
-    return this.http.get<MAPA_PHNMSIPA[]>(this.apiUrl).pipe(
+    return this.http.get<MAPA_PHNMSIPA[]>(`${this.apiUrl}listado-pagos`).pipe(
       tap(() => console.log('conectado a python'))
     );
+  }
+  getCabecera(): Observable<any>{
+    return this.http.get(`${this.apiUrl}cabecera`)
+  }
+  getAyuda(manual: string): Observable<any>{
+    return this.http.get(`${this.apiUrl}ayuda/${manual}`)
   }
 }
