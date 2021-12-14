6f9cc1c6c0c79cb7ecc27f6dab3848c1431f9f25
Oscar Vasquez <oscardavid.vp@gmail.com>
2021-12-14 02:55:06 +0100

    control de PFs ahora en app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'phr';
+export class AppComponent implements OnInit{
+  title = 'POC';
+
+  ngOnInit(): void {
+    // listado de teclas de funcion
+    let pfs = ['F1','F2','F3','F4','F5','F6','F7','F8']
+
+    // no funciona con keypress
+    window.addEventListener("keyup", disableF5);
+    window.addEventListener("keydown", disableF5);
+
+    // https://stackoverflow.com/questions/42790602/how-do-i-check-whether-an-array-contains-a-string-in-typescript
+    // https://www.itsolutionstuff.com/post/how-to-disable-browser-refresh-button-in-angularexample.html
+
+    function disableF5(e: { which: any; key: any; preventDefault: () => void; }) { //key: any -> keycode: any
+    console.log(e);
+      // if ((e.which || e.keyCode) == ( 112 || 113 || 114 || 115 || 116 )) e.preventDefault();
+      if (pfs.includes(e.key)) e.preventDefault();
+
+    };
+  }
+
 }
--- src/app/phnmali1/phnmali1.component.ts
+++ src/app/phnmali1/phnmali1.component.ts
@@ -34,24 +34,9 @@ export class Phnmali1Component implements OnInit {
   constructor(public dialog: MatDialog) { }
 
   ngOnInit(){
-    // listado de teclas de funcion
-    let pfs = ['F1','F2','F3','F4','F5','F6']
 
-    // no funciona con keypress
-    window.addEventListener("keyup", disableF5);
-    window.addEventListener("keydown", disableF5);
+  }
 
-    // https://stackoverflow.com/questions/42790602/how-do-i-check-whether-an-array-contains-a-string-in-typescript
-    // https://www.itsolutionstuff.com/post/how-to-disable-browser-refresh-button-in-angularexample.html
-
-   function disableF5(e: { which: any; key: any; preventDefault: () => void; }) { //key: any -> keycode: any
-    console.log(e);
-      // if ((e.which || e.keyCode) == ( 112 || 113 || 114 || 115 || 116 )) e.preventDefault();
-      if (pfs.includes(e.key)) e.preventDefault();
-
-   };
-
- }
   openDialog() {
     this.dialog.open(CabeceraComponent);
   }
